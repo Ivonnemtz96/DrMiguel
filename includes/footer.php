1f77d98f3757169a428270244dcc5d1682c8b7d9
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-15 14:22:23 -0700

    diseño a servidor

---
+++ includes/footer.php
@@ -0,0 +1,161 @@
+<footer class="footer">
+    <div class="footer-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-3">
+                    <div class="footer-widget-about">
+                        <img src="assets/images/logo/logo-light.png" alt="logo">
+                    </div><!-- /.footer-widget-about -->
+                </div><!-- /.col-lg-3 -->
+                <div class="col-6 col-lg-2">
+                    <div class="footer-widget-nav">
+                        <h6 class="footer-widget-title">About Us</h6>
+                        <nav>
+                            <ul class="list-unstyled">
+                                <li><a href="about-us.html">About Us</a></li>
+                                <li><a href="team.html">Leadership Team</a></li>
+                                <li><a href="blog.html">News & Media</a></li>
+                                <li><a href="services.html">Sustainability</a></li>
+                                <li><a href="careers.html">Careers</a></li>
+                            </ul>
+                        </nav>
+                    </div><!-- /.footer-widget-content -->
+                </div><!-- /.col-lg-2 -->
+                <div class="col-6 col-lg-2">
+                    <div class="footer-widget-nav">
+                        <h6 class="footer-widget-title">Tests & Services</h6>
+                        <nav>
+                            <ul class="list-unstyled">
+                                <li><a href="#">General Diagnostic Tests</a></li>
+                                <li><a href="#"> Specialized Genetic Tests</a></li>
+                                <li><a href="#"> Food Sensitivity Tests</a></li>
+                                <li><a href="#">Genova Diagnostics </a></li>
+                                <li><a href="#">Hormone Insights Tests</a></li>
+                                <li><a href="#">Naturopathic Tests</a></li>
+                            </ul>
+                        </nav>
+                    </div><!-- /.footer-widget-content -->
+                </div><!-- /.col-lg-2 -->
+                <div class="col-6 col-lg-2">
+                    <div class="footer-widget-nav">
+                        <h6 class="footer-widget-title">Links</h6>
+                        <nav>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Knowledge base</a></li>
+                                <li><a href="#">Report a Vulnerability</a></li>
+                                <li><a href="#">Suppliers & Vendors</a></li>
+                                <li><a href="#">Genova Diagnostics </a></li>
+                                <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                        </nav>
+                    </div><!-- /.footer-widget-content -->
+                </div><!-- /.col-lg-2 -->
+                <div class="col-6 col-lg-3">
+                    <div class="footer-widget-contact">
+                        <h6 class="footer-widget-title">Quick Contacts</h6>
+                        <p>If you have any questions or need help, feel free to contact with our team.</p>
+                        <ul class="contact-list list-unstyled">
+                            <li>
+                                <p>Send Us Email:</p>
+                                <a
+                                    href="https://7oroof.com/cdn-cgi/l/email-protection#97f4fff2fafbf6f5e4d7a0f8e5f8f8f1b9f4f8fa">
+                                    <i class="contact-icon icon-email"></i> <span><span class="__cf_email__"
+                                            data-cfemail="ccafa4a9a1a0adaebf8cfba3bea3a3aae2afa3a1">[email&#160;protected]</span></span>
+                                </a>
+                            </li>
+                            <li>
+                                <p>Call Us Today:</p>
+                                <a href="tel:00201061245741">
+                                    <i class="contact-icon icon-phone"></i> <span>(002) 01061245741</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- /.col-lg-3 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.footer-primary -->
+    <div class="footer-secondary">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm-12 col-md-12 col-lg-7">
+                    <nav>
+                        <ul class="list-unstyled footer-copyright-links d-flex flex-wrap mb-0">
+                            <li><a href="#">Privacy Statement</a></li>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Notice of Nondiscrimination</a></li>
+                            <li><a href="#">Report a Vulnerability</a></li>
+                        </ul>
+                    </nav>
+                    <div class="mt-1">
+                        <span>&copy; 2023 Chemlabs, All Rights Reserved. With Love by </span>
+                        <a href="http://themeforest.net/user/7oroof">7oroof.com</a>
+                    </div>
+                </div><!-- /.col-lg-6 -->
+                <div class="col-sm-12 col-md-12 col-lg-5 d-flex flex-wrap justify-content-between">
+                    <ul class="social-icons list-unstyled mb-0 mr-50">
+                        <li>
+                            <a href="#"><i class="social-icon icon-facebook"></i><span
+                                    class="social-title">Facebook</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="social-icon icon-twitter"></i><span
+                                    class="social-title">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="social-icon icon-youtube"></i><span
+                                    class="social-title">Youtube</span></a>
+                        </li>
+                    </ul><!-- /.social-icons -->
+                    <button id="scrollTopBtn">
+                        <span class="btn-text">Back To Top </span> <i class="icon-arrow-long"></i>
+                    </button>
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.footer-secondary -->
+</footer><!-- /.Footer -->
+
+<svg class="svg-pathes" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <defs>
+        <filter id="rounded-clippath-filter">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur"></feGaussianBlur>
+            <feColorMatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                result="rounded-clippath-filter">
+            </feColorMatrix>
+            <feComposite in="SourceGraphic" in2="rounded-clippath-filter" operator="atop"></feComposite>
+        </filter>
+    </defs>
+    <defs>
+        <filter id="rounded-clippath-filter-small">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur"></feGaussianBlur>
+            <feColorMatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 10 -4"
+                result="rounded-clippath-filter-small"></feColorMatrix>
+            <feComposite in="SourceGraphic" in2="rounded-clippath-filter-small" operator="atop"></feComposite>
+        </filter>
+    </defs>
+    <clipPath id="curve-left" clipPathUnits="objectBoundingBox">
+        <path
+            d="M0,0.998 v0.002 H1 V0 h-0.059 C0.832,0.008,0.596,0.068,0.48,0.414 A1,1,0,0,0,0.445,0.551 C0.376,0.899,0.166,0.986,0,0.998">
+        </path>
+    </clipPath>
+    <clipPath id="curve-right" clipPathUnits="objectBoundingBox">
+        <path
+            d="M1,0.998 v0.002 H0 V0 H0.059 C0.168,0.008,0.404,0.068,0.519,0.414 A1,1,0,0,1,0.555,0.551 C0.624,0.899,0.834,0.986,1,0.998">
+        </path>
+    </clipPath>
+    <clipPath id="curve-shape" clipPathUnits="objectBoundingBox">
+        <path d="M1600,3277.99c-152.02-27.95-453.41-46.99-800.382-46.99-346.4,0-647.36,19.12-799.618,47Z"
+            transform="translate(0 -3231)">
+        </path>
+    </clipPath>
+</svg>
+
+<div class="search-popup">
+    <button type="button" class="search-popup-close"><i class="fas fa-times"></i></button>
+    <form class="search-popup-form">
+        <input type="text" class="search-popup-form-input" placeholder="Type Words Then Enter">
+        <button class="search-popup-btn"><i class="icon-search"></i></button>
+    </form>
+</div>
---
+++ includes/head.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="description" content="Chemlabs - Laboratory & Science Research HTML5 Template">
+    <link href="assets/images/favicon/favicon.png" rel="icon">
+    <title>Dr. Miguel Blanco</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;700&amp;family=Roboto:wght@400;700&amp;display=swap">
+    <link rel="stylesheet" href="use.fontawesome.com/releases/v5.15.1/css/all.css">
+    <link rel="stylesheet" href="assets/css/libraries.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
