eb1e839670ce5dd22de6db6cb95dee5d5d131470
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-16 10:58:53 -0700

    se agrega favicon

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content="Chemlabs - Laboratory & Science Research HTML5 Template">
-    <link href="assets/images/favicon/favicon.png" rel="icon">
+    <link href="assets/images/favicon/favicon2.png" rel="icon">
     <title>Dr. Miguel Blanco</title>
 
     <link rel="stylesheet"
